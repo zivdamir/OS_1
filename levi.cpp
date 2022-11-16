2ce2b7f580b677d93de3f6127564058a3ec14ff6
Ziv Damir <zivdamir@campus.technion.ac.il>
2022-11-16 19:36:50 +0000

    hello

--- levi.cpp
+++ levi.cpp
@@ -3,3 +3,5 @@ hello Levi , how are you?
 ziv, I am sorry for calling you damir
 its okay, levi, but because you called me damir you have to do all of the wet all by yourself
 goodbye.
+
+lol look how i do it
